3e0d9faddccc09cc830beac59e58cbb4b73d1010
Bridger Thompson <bridgerat00@gmail.com>
2023-09-29 12:14:06 -0600

    navbar

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,3 @@
-// Your main application file (e.g., App.tsx)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/custom.scss";
@@ -6,6 +5,7 @@ import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { App } from "./App";
+import { NavBar } from "./components/NavBar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,6 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
