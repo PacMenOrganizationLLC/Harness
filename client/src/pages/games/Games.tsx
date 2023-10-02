cbc606e79f087ba842f98e3809b83cfab095f378
Bridger Thompson <bridgerat00@gmail.com>
2023-10-02 16:58:17 -0600

    game

--- client/src/pages/games/Games.tsx
+++ client/src/pages/games/Games.tsx
@@ -3,12 +3,11 @@ import { useGetGamesQuery } from "./gameHooks";
 export const Games = () => {
   const gamesQuery = useGetGamesQuery();
   const games = gamesQuery.data ?? [];
-  console.log(games);
   return (
     <div className="container">
       <h1 className="text-center">Games</h1>
-      {games.map((g: any) => (
-        <div>{g.date}</div>
+      {games.map((g) => (
+        <div>{g.name}</div>
       ))}
     </div>
   );
