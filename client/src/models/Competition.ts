86efadfb9d00e4485b517fa39eb7a67a31d42897
Anthony Hardman <anthonythardman@gmail.com>
2023-10-23 13:33:35 -0600

    competition model changes

--- client/src/models/Competition.ts
+++ client/src/models/Competition.ts
@@ -1,3 +1,15 @@
+import { Game } from "./Games";
+import Event from "./Event"
+
+export interface CompetitionPrize {
+  id: number;
+  prize: string;
+  competitionId: number;
+  imageFileName: string;
+  placement: number;
+  userId?: string;
+}
+
 export interface Competition {
   id: number;
   gameId: number;
@@ -5,4 +17,7 @@ export interface Competition {
   startAt: Date;
   endAt: Date;
   location: string;
+  game?: Game
+  event?: Event
+  prizes?: CompetitionPrize[];
 }
