ce1fee19aacac5c5f9b5879d97fc75c2f10238d8
Bridger Thompson <bridgerat00@gmail.com>
2023-10-26 09:22:46 -0600

    Consistent page stylings

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -4,6 +4,7 @@ import { useGetEventsQuery } from "./eventHooks";
 import Event from "../../models/Event";
 import { EventEditorModal } from "./EventEditorModal";
 import { EventDetails } from "./EventDetails";
+import { Spinner } from "../../components/Spinner";
 export const Events = () => {
   const eventsQuery = useGetEventsQuery();
   const [selectedEvent, setSelectedEvent] = useState<Event>()
@@ -14,32 +15,31 @@ export const Events = () => {
     }
   }, [eventsQuery.data])
 
+  if (eventsQuery.isLoading) return <Spinner />
+  if (eventsQuery.isError) return <h3 className="text-center">Error getting events</h3>
+  if (!eventsQuery.data) return <h3 className="text-center">Unable to get events</h3>
+
   return (
     <div className="container">
+      <h1 className="text-center">Events</h1>
       <div className="row">
-        <div className="col">
-          <h1 className="text-center">Events</h1>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="text-end mb-2 d-md-none">
-                <EventEditorModal setSelectedEvent={setSelectedEvent} />
-              </div>
-              {eventsQuery.data && (
-                <EventList events={eventsQuery.data} selectedEvent={selectedEvent} setSelectedEvent={setSelectedEvent} />
-              )}
-              <div className="text-center d-none d-md-block">
-                <EventEditorModal setSelectedEvent={setSelectedEvent} />
-              </div>
-            </div>
-            <div className="col-md-8">
-              {selectedEvent && (
-                <EventDetails selectedEvent={selectedEvent}
-                  setSelectedEvent={setSelectedEvent} />
-              )}
-            </div>
+        <div className="col-md-4 col-12">
+          <div className="text-end mb-2 d-md-none">
+            <EventEditorModal setSelectedEvent={setSelectedEvent} />
+          </div>
+          <div className="mb-2 mb-md-0">
+            <EventList events={eventsQuery.data}
+              selectedEvent={selectedEvent}
+              setSelectedEvent={setSelectedEvent} />
+          </div>
+          <div className="text-center my-2 d-none d-md-block">
+            <EventEditorModal setSelectedEvent={setSelectedEvent} />
           </div>
         </div>
-
+        {selectedEvent && (
+          <EventDetails selectedEvent={selectedEvent}
+            setSelectedEvent={setSelectedEvent} />
+        )}
       </div>
     </div>
   );
--- client/src/pages/games/sessionConfig/SessionConfigList.tsx
+++ client/src/pages/games/sessionConfig/SessionConfigList.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react"
 import { useGetSessionConfigsQuery } from "./sessionConfigHooks";
 import { Spinner } from "../../../components/Spinner";
-import { AddSessionConfigModal } from "./AddSessionConfigModal";
 import { ConfigDetailsModal } from "./ConfigDetailsModal";
 
-export const SessionConfig: FC<{
+export const SessionConfigList: FC<{
   gameId: number
 }> = ({ gameId }) => {
   const sessionConfigsQuery = useGetSessionConfigsQuery(gameId);
@@ -15,17 +14,12 @@ export const SessionConfig: FC<{
   if (!sessionConfigsQuery.data) return <div>Unable to get session configurations</div>
 
   return (
-    <div className="row mt-3">
+    <div className="row">
       {sessionConfigs.map((c) => (
         <div className="col-lg-3 col-md-6 col-12 my-1 px-1">
           <ConfigDetailsModal config={c} gameId={gameId} key={c.id} />
         </div>
       ))}
-      <div className="col-auto my-auto px-1">
-        <div className="card">
-          <AddSessionConfigModal gameId={gameId} />
-        </div>
-      </div>
-    </div>
+    </div >
   )
 }
