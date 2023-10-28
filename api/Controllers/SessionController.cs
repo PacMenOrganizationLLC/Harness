57274e918c97bafaf6de5160412367457096897e
Joshua Barlow <jalapenojosh@gmail.com>
2023-10-28 13:48:43 -0600

    Added a controller endpoint that is hardcoded to start a session of our mars rover

--- api/Controllers/SessionController.cs
+++ api/Controllers/SessionController.cs
@@ -6,98 +6,110 @@ using Microsoft.EntityFrameworkCore;
 [ApiController]
 public class SessionController : ControllerBase
 {
-  private readonly HarnessContext _context;
+    private readonly HarnessContext _context;
+    private HttpClient _httpClient;
 
-  public SessionController(HarnessContext context)
-  {
-    _context = context;
-  }
+    public SessionController(HarnessContext context)
+    {
+        _context = context;
+        _httpClient = new HttpClient();
+    }
 
-  [HttpPost]
-  public async Task<IActionResult> AddSessionAsync(Session session)
-  {
-    _context.Session.Add(session);
+    [HttpPost]
+    public async Task<IActionResult> AddSessionAsync(Session session)
+    {
+        _context.Session.Add(session);
 
-    await _context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
 
-    return Ok("Added Session Successfully");
-  }
+        return Ok("Added Session Successfully");
+    }
 
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<Session>>> GetSessionsAsync()
-  {
-    List<Session> sessions = await _context.Session.ToListAsync();
-    return sessions;
-  }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Session>>> GetSessionsAsync()
+    {
+        List<Session> sessions = await _context.Session.ToListAsync();
+        return sessions;
+    }
 
-  [HttpGet("{id}")]
-  public async Task<ActionResult<Session>> GetSessionAsync(int id)
-  {
-    Session? session = await _context.Session.Where((s) => s.Id == id).FirstOrDefaultAsync();
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Session>> GetSessionAsync(int id)
+    {
+        Session? session = await _context.Session.Where((s) => s.Id == id).FirstOrDefaultAsync();
 
-    if (session == null)
-      return NotFound();
+        if (session == null)
+            return NotFound();
 
-    return session;
-  }
+        return session;
+    }
+    [HttpPut("createSession/{gameId}")]
+    public async Task<string> CreateSessionAsync(int gameId)
+    {
+        // Replace with your actual API endpoint
+        string apiUrl = "https://marswebpacmen.azurewebsites.net/Admin/createSession";
 
+        // Make the API call and get the response
+        string myResponse = await _httpClient.GetFromJsonAsync<string>(apiUrl);
 
-   // [HttpPut("createSession/{gameId}")]
-   // public async Task<string> CreateSessionAsync(int gameId)
-   // {
-        //TODO: 
-        //1. set it up to make a session for mars rover
-        //2. figure out how the whole endpoints thing works
-        //3. Make it work for any game provided it has the endpoint
-   // }
-
+        // Check if the call was successful
+        if (myResponse == null || myResponse == "")
+        {
+            // Read and return the response content as a string
+            return await "error: something's wrong";
+        }
+        else
+        {
+            // Handle the error case (you can throw an exception or return an error message)
+            return myResponse;
+        }
+    }
 
 
     [HttpPut("{startSessionId}")]
-  public async Task<IActionResult> StartSessionAsync(int id)
-  {
-    Session? session = await _context.Session.FindAsync(id);
+    public async Task<IActionResult> StartSessionAsync(int id)
+    {
+        Session? session = await _context.Session.FindAsync(id);
 
-    if (session == null)
-      return NotFound();
+        if (session == null)
+            return NotFound();
 
-    //TODO: Call game API to start game. Pass in config
+        //TODO: Call game API to start game. Pass in config
 
-    await _context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
 
-    return Ok("Session has been started");
-  }
-
-
+        return Ok("Session has been started");
+    }
 
 
 
-  [HttpPut("{stopSessionId}")]
-  public async Task<IActionResult> StopSessionAsync(int id)
-  {
-    Session? session = await _context.Session.FindAsync(id);
 
-    if (session == null)
-      return NotFound();
 
-    //TODO: Call game API to stop game.
+    [HttpPut("{stopSessionId}")]
+    public async Task<IActionResult> StopSessionAsync(int id)
+    {
+        Session? session = await _context.Session.FindAsync(id);
 
-    await _context.SaveChangesAsync();
+        if (session == null)
+            return NotFound();
 
-    return Ok("Session has been stopped");
-  }
+        //TODO: Call game API to stop game.
 
-  [HttpDelete("{id}")]
-  public async Task<IActionResult> DeleteSessionAsync(int id)
-  {
-    Session? session = await _context.Session.FindAsync(id);
+        await _context.SaveChangesAsync();
 
-    if (session == null)
-      return NotFound();
+        return Ok("Session has been stopped");
+    }
 
-    _context.Session.Remove(session);
-    await _context.SaveChangesAsync();
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSessionAsync(int id)
+    {
+        Session? session = await _context.Session.FindAsync(id);
 
-    return Ok("Session Deleted Successfully");
-  }
+        if (session == null)
+            return NotFound();
+
+        _context.Session.Remove(session);
+        await _context.SaveChangesAsync();
+
+        return Ok("Session Deleted Successfully");
+    }
 }
