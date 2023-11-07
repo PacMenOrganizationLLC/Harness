ec46edb5d68ff68808720aa961fc06a3df5f02f8
Anthony Hardman <anthonythardman@gmail.com>
2023-11-07 15:39:26 -0700

    forgot add --all

---
+++ api/models/SessionScoreboard.cs
@@ -0,0 +1,11 @@
+﻿namespace api.models
+{
+    public class SessionScoreboard
+    {
+        public int Id { get; set; }
+        public int SessionId { get; set; }
+        public string PlayerName { get; set; }
+        public int Rank { get; set; }
+        public int? Score { get; set; }
+    }
+}
