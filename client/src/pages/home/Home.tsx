9724b3ce622faf4c5e91eaafa14567c1f995de50
Joshua Barlow <jalapenojosh@gmail.com>
2023-11-11 23:15:26 -0700

    added carousel, doesn't work but also doesn't break everything

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "../../components/Spinner";
 import { useGetEventsQuery, useGetPastEventsQuery } from "../events/eventHooks";
 
 export const Home = () => {
@@ -5,25 +6,46 @@ export const Home = () => {
   const getEventsQuery = useGetEventsQuery();
   const pastEvents = getPastEventsQuery.data ?? [];
   const events = getEventsQuery.data ?? [];
+  if (getEventsQuery.isLoading || getPastEventsQuery.isLoading)
+    return <Spinner />;
+  if (getEventsQuery.isError || getPastEventsQuery.isError)
+    return <h3 className="text-center">Error getting events</h3>;
+  if (!getEventsQuery.data || !getEventsQuery.data)
+    return <h3 className="text-center">Unable to get games</h3>;
 
   return (
-    <div className="row">
-      <div className="col">
+    <>
+      <div className="row">
         <h1>Past Events</h1>
         {pastEvents.map((e) => (
-          <div>
-            <h3>{e.name}</h3>
-          </div>
+          <h2>{e.name}</h2>
         ))}
+        <div className="carousel slide">
+          <div className="carousel-inner">
+            {pastEvents.map((e) => (
+              <div className="carousel-item">
+                <h2>{e.name}</h2>
+                <h1>{new Date(e.day).toDateString()}</h1>
+                <h1>{e.location}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="col">
+      <div className="row">
         <h1>Events</h1>
-        {events.map((e) => (
-          <div>
-            <h3>{e.name}</h3>
+        <div className="carousel slide">
+          <div className="carousel-inner">
+            {events.map((e) => (
+              <div className="carousel-item">
+                <h2>{e.name}</h2>
+                <h1>{new Date(e.day).toDateString()}</h1>
+                <h1>{e.location}</h1>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
