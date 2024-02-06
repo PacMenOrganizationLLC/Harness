fd41ed32a4d133c0fd3e41892a9c44e3ed0babbb
Joshua Barlow <jalapenojosh@gmail.com>
2024-02-06 12:54:43 -0700

    limited scoreboard to display only top 10

--- client/src/components/ScoreboardDisplay.tsx
+++ client/src/components/ScoreboardDisplay.tsx
@@ -5,7 +5,9 @@ interface ScoreboardComponentProps {
   scoreBoard: SessionScoreboard[];
 }
 
-export const ScoreboardComponent: FC<ScoreboardComponentProps> = ({ scoreBoard }) => {
+export const ScoreboardComponent: FC<ScoreboardComponentProps> = ({
+  scoreBoard,
+}) => {
   const filteredScoreboard = scoreBoard.filter((s) => s.rank <= 10);
   return (
     <div className="">
@@ -14,13 +16,23 @@ export const ScoreboardComponent: FC<ScoreboardComponentProps> = ({ scoreBoard }
         <div className="col text-start px-0">Player</div>
         <div className="col px-0">Score</div>
       </div>
-      {filteredScoreboard.map((s, index) => (
-        <div className={`row ${index < filteredScoreboard.length - 1 && "border-bottom"} py-1`} key={s.rank}>
-          <div className="col-3 px-0">{s.rank}</div>
-          <div className="col text-truncate text-start px-0">{s.playerName}</div>
-          <div className="col px-0">{s.score}</div>
-        </div>
-      ))}
+      {filteredScoreboard.map(
+        (s, index) =>
+          s.rank <= 10 && (
+            <div
+              className={`row ${
+                index < filteredScoreboard.length - 1 ? "border-bottom" : ""
+              } py-1`}
+              key={s.rank}
+            >
+              <div className="col-3 px-0">{s.rank}</div>
+              <div className="col text-truncate text-start px-0">
+                {s.playerName}
+              </div>
+              <div className="col px-0">{s.score}</div>
+            </div>
+          )
+      )}
     </div>
   );
 };
