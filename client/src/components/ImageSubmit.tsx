8ea03eafeacc77aa7d9b288e0fa8080398131388
Joshua Barlow <jalapenojosh@gmail.com>
2024-02-07 20:29:55 -0700

    got the image to the modal

--- client/src/components/ImageSubmit.tsx
+++ client/src/components/ImageSubmit.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useState } from "react";
 
-const ImageUploader: FC<{}> = () => {
+const ImageUploader: FC<{
+  setConvertedSrc: (i?: string) => void;
+}> = ({ setConvertedSrc }) => {
   const [imageSrc, setImageSrc] = useState<string>("");
-  const [convertedSrc, setConvertedSrc] = useState<string>("");
+  //   const [convertedSrc, setConvertedSrc] = useState<string>("");
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -30,20 +32,9 @@ const ImageUploader: FC<{}> = () => {
       <input type="file" accept="image/*" onChange={handleImageChange} />
       {imageSrc && (
         <div>
-          <h2>Original Image:</h2>
           <img
             src={imageSrc}
-            alt="Original"
-            style={{ maxWidth: "100%", maxHeight: "200px" }}
-          />
-        </div>
-      )}
-      {convertedSrc && (
-        <div>
-          <h2>Converted Image for Transfer:</h2>
-          <img
-            src={`data:image/jpeg;base64,${convertedSrc}`}
-            alt="Converted"
+            alt="Selected Choice"
             style={{ maxWidth: "100%", maxHeight: "200px" }}
           />
         </div>
