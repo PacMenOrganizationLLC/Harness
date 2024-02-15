a9ee08762cc5f7bb8d66d831190796668bb6e6d4
Joshua Barlow <jalapenojosh@gmail.com>
2024-02-14 20:15:33 -0700

    Added images to more spots

--- client/src/pages/home/GameInfo.tsx
+++ client/src/pages/home/GameInfo.tsx
@@ -8,14 +8,16 @@ export const GameInfo = () => {
   const navigate = useNavigate();
   const gameId = useParams<{ id: string }>().id;
   const gameQuery = useGetGameQuery(gameId);
-  const game = gameQuery.data
+  const game = gameQuery.data;
   const competitionsQuery = useGetCompetitionsByGameQuery(gameId);
   const upcomingCompetitions = competitionsQuery.data ?? [];
 
-  if (gameQuery.isLoading || competitionsQuery.isLoading) return <Spinner />
-  if (gameQuery.isError) return <h3 className="text-center">Error getting game</h3>
-  if (competitionsQuery.isError) return <h3 className="text-center">Error getting competitions</h3>
-  if (!game) return <h3 className="text-center">Unable to get game</h3>
+  if (gameQuery.isLoading || competitionsQuery.isLoading) return <Spinner />;
+  if (gameQuery.isError)
+    return <h3 className="text-center">Error getting game</h3>;
+  if (competitionsQuery.isError)
+    return <h3 className="text-center">Error getting competitions</h3>;
+  if (!game) return <h3 className="text-center">Unable to get game</h3>;
 
   return (
     <div className="container mt-2">
@@ -30,22 +32,26 @@ export const GameInfo = () => {
         </div>
         <div className="col my-auto text-end">
           {game.repoLink && (
-            <a href={game.repoLink}
+            <a
+              href={game.repoLink}
               target="_blank"
               rel="noreferrer"
               className="text-decoration-none fs-4"
             >
-              <i className="bi-github pe-1" />Github
+              <i className="bi-github pe-1" />
+              Github
             </a>
           )}
         </div>
         <div className="col my-auto">
-          <a href="https://marswebpacmen.azurewebsites.net/swagger/index.html"
+          <a
+            href="https://marswebpacmen.azurewebsites.net/swagger/index.html"
             target="_blank"
             rel="noreferrer"
             className="text-decoration-none fs-4"
           >
-            <i className="bi-box-arrow-up-right pe-1" />Swagger
+            <i className="bi-box-arrow-up-right pe-1" />
+            Swagger
           </a>
         </div>
       </div>
@@ -53,13 +59,43 @@ export const GameInfo = () => {
         <div className="col">
           <div className="fs-4 text-center">How to Join</div>
           <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Velit scelerisque in dictum non consectetur a erat. Elit duis tristique sollicitudin nibh. Leo in vitae turpis massa. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Non nisi est sit amet facilisis magna. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Lacus viverra vitae congue eu consequat ac felis donec. Netus et malesuada fames ac turpis egestas sed tempus urna. Eget nunc scelerisque viverra mauris. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Et malesuada fames ac turpis egestas integer eget. Faucibus purus in massa tempor nec. Semper quis lectus nulla at volutpat diam. Dictum sit amet justo donec enim. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Donec massa sapien faucibus et molestie ac feugiat sed.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Condimentum id venenatis a condimentum vitae sapien pellentesque
+            habitant. Velit scelerisque in dictum non consectetur a erat. Elit
+            duis tristique sollicitudin nibh. Leo in vitae turpis massa. Etiam
+            dignissim diam quis enim lobortis scelerisque fermentum dui. Non
+            nisi est sit amet facilisis magna. Erat pellentesque adipiscing
+            commodo elit at imperdiet dui accumsan. Lacus viverra vitae congue
+            eu consequat ac felis donec. Netus et malesuada fames ac turpis
+            egestas sed tempus urna. Eget nunc scelerisque viverra mauris. Nisl
+            condimentum id venenatis a condimentum vitae sapien pellentesque. Et
+            malesuada fames ac turpis egestas integer eget. Faucibus purus in
+            massa tempor nec. Semper quis lectus nulla at volutpat diam. Dictum
+            sit amet justo donec enim. Odio pellentesque diam volutpat commodo
+            sed egestas egestas fringilla. Donec massa sapien faucibus et
+            molestie ac feugiat sed.
           </div>
         </div>
         <div className="col">
           <div className="fs-4 text-center">How to Play</div>
           <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Velit scelerisque in dictum non consectetur a erat. Elit duis tristique sollicitudin nibh. Leo in vitae turpis massa. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Non nisi est sit amet facilisis magna. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Lacus viverra vitae congue eu consequat ac felis donec. Netus et malesuada fames ac turpis egestas sed tempus urna. Eget nunc scelerisque viverra mauris. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Et malesuada fames ac turpis egestas integer eget. Faucibus purus in massa tempor nec. Semper quis lectus nulla at volutpat diam. Dictum sit amet justo donec enim. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Donec massa sapien faucibus et molestie ac feugiat sed.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Condimentum id venenatis a condimentum vitae sapien pellentesque
+            habitant. Velit scelerisque in dictum non consectetur a erat. Elit
+            duis tristique sollicitudin nibh. Leo in vitae turpis massa. Etiam
+            dignissim diam quis enim lobortis scelerisque fermentum dui. Non
+            nisi est sit amet facilisis magna. Erat pellentesque adipiscing
+            commodo elit at imperdiet dui accumsan. Lacus viverra vitae congue
+            eu consequat ac felis donec. Netus et malesuada fames ac turpis
+            egestas sed tempus urna. Eget nunc scelerisque viverra mauris. Nisl
+            condimentum id venenatis a condimentum vitae sapien pellentesque. Et
+            malesuada fames ac turpis egestas integer eget. Faucibus purus in
+            massa tempor nec. Semper quis lectus nulla at volutpat diam. Dictum
+            sit amet justo donec enim. Odio pellentesque diam volutpat commodo
+            sed egestas egestas fringilla. Donec massa sapien faucibus et
+            molestie ac feugiat sed.
           </div>
         </div>
       </div>
@@ -67,23 +103,43 @@ export const GameInfo = () => {
       <div className="row">
         {upcomingCompetitions.map((c, index) => (
           <div key={index} className="col-12 col-md-6 col-lg-4 col-xl-3 my-1">
-            <Link to={`/competition/${c.id}`}
-              className="text-reset text-decoration-none">
+            <Link
+              to={`/competition/${c.id}`}
+              className="text-reset text-decoration-none"
+            >
               <div className="card text-center h-100 bg-secondary-subtle">
                 <div className="card-body">
                   <div className="card-title fw-bold">{c.name}</div>
-                  {new Date(c.startAt).toDateString() === new Date(c.endAt).toDateString() ? (
+
+                  {new Date(c.startAt).toDateString() ===
+                  new Date(c.endAt).toDateString() ? (
                     <div>{FormatDate(c.startAt)}</div>
                   ) : (
-                    <div>{FormatDate(c.startAt)} - {FormatDate(c.endAt)}</div>
+                    <div>
+                      {FormatDate(c.startAt)} - {FormatDate(c.endAt)}
+                    </div>
                   )}
-                  <div>{getTimeNoSeconds(c.startAt)} - {getTimeNoSeconds(c.endAt)}</div>
+                  <div>
+                    {getTimeNoSeconds(c.startAt)} - {getTimeNoSeconds(c.endAt)}
+                  </div>
                 </div>
               </div>
-            </Link >
+            </Link>
           </div>
         ))}
+        {game.imageSource && (
+          <div className="m-2">
+            <img
+              src={
+                process.env.REACT_APP_API_URL +
+                "/api/Game/Image/" +
+                game.imageSource
+              }
+              alt="Game"
+            />
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
