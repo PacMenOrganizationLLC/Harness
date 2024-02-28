64bfc125291329600ef507c62cb28f6e7c295e4c
anthony <anthonythardman@gmail.com>
2024-02-28 13:11:00 -0700

    added docker api and traefik to local dev docker compose

--- docker-api/main.py
+++ docker-api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 client = docker.from_env()
 
 
-@app.get('/getContainer/{delay}')
+@app.get("/getContainer/{delay}")
 async def get_container(delay: int = 30):
     min_external_port = 3000  # Minimum standard external port
     max_external_port = 8500  # Maximum standard external port
@@ -21,24 +21,24 @@ async def get_container(delay: int = 30):
         container.remove()
 
     container_config = {
-        'image': 'nginx',
-        'detach': True,
-        'ports': {f'{internal_port}/tcp': external_port},
-        'labels': {
+        "image": "nginx",
+        "detach": True,
+        "ports": {f"{internal_port}/tcp": external_port},
+        "labels": {
             "traefik.enable": "true",
             "traefik.http.middlewares.mars-stripprefix.stripprefix.prefixes": "/mars",
             "traefik.http.routers.mars.entrypoints": "web",
             "traefik.http.routers.mars.rule": "PathPrefix(`/mars`)",
-            "traefik.http.routers.mars.middlewares": "mars-stripprefix@docker"
+            "traefik.http.routers.mars.middlewares": "mars-stripprefix@docker",
         },
-        'environment': {'DEBUG': 'true', 'LOG_LEVEL': 'verbose'}
+        "environment": {"DEBUG": "true", "LOG_LEVEL": "verbose"},
     }
 
     container = client.containers.run(**container_config)
 
     container_info = container.attrs
-    container_ip = container_info['NetworkSettings']['IPAddress']
+    container_ip = container_info["NetworkSettings"]["IPAddress"]
     asyncio.create_task(endContainer(container, delay))
-    url = f'144.17.92.11:{external_port}'
+    url = f"144.17.92.11:{external_port}"
 
-    return f'Container is running at: {url} with delay set to {delay} seconds'
+    return f"Container is running at: {url} with delay set to {delay} seconds"
