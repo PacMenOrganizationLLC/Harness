2da89a60f20ad7737bf692991ddffade3f5eb9e8
anthony <anthonythardman@gmail.com>
2024-03-13 19:27:44 -0600

    api side auth

--- client/src/pages/games/gameService.ts
+++ client/src/pages/games/gameService.ts
@@ -2,29 +2,30 @@ import axios from "axios";
 import { DockerConfig, Game } from "../../models/Game";
 import { GameDto } from "../../models/GameDto";
 import toast from "react-hot-toast";
+import { useAuth } from "react-oidc-context";
 
 const BaseUrl = process.env.REACT_APP_API_URL;
 
 export const gameService = {
   async getGames(): Promise<Game[]> {
-    const url = `${BaseUrl}/api/Game`;
+    const url = `/api/Game`;
     const response = await axios.get(url);
     return response.data;
   },
   async getGame(id: number): Promise<Game> {
-    const url = `${BaseUrl}/api/Game/${id}`;
+    const url = `/api/Game/${id}`;
     const response = await axios.get(url);
     return response.data;
   },
   async addGame(game: GameDto): Promise<number> {
     const addGame: Game = await convertDtoToGame(game);
-    const url = `${BaseUrl}/api/Game`;
+    const url = `/api/Game`;
     while (!addGame) { }
     const response = await axios.post(url, addGame);
     return response.data;
   },
   async addInstructions(gameId: number, rules: string, gettingStarted: string) {
-    const url = `${BaseUrl}/api/Game/${gameId}/instructions`
+    const url = `/api/Game/${gameId}/instructions`
     const instructions = {
       GameRules: rules,
       GettingStarted: gettingStarted
@@ -34,14 +35,14 @@ export const gameService = {
   },
   async updateGame(game: GameDto) {
     const myGame: Game = await convertDtoToGame(game);
-    const url = `${BaseUrl}/api/Game/${game.id}`;
+    const url = `/api/Game/${game.id}`;
     while (!myGame) { }
     console.log(myGame);
     const response = await axios.put(url, myGame);
     return response.data;
   },
   async addDockerConfig(gameId: number, dockerConfig: DockerConfig) {
-    const url = `${BaseUrl}/api/Game/${gameId}/docker`
+    const url = `/api/Game/${gameId}/docker`
     const body = {
       DockerImage: dockerConfig.dockerImage,
       Duration: dockerConfig.duration,
@@ -53,12 +54,12 @@ export const gameService = {
     return response.data
   },
   async deleteGame(id: number) {
-    const url = `${BaseUrl}/api/Game/${id}`;
+    const url = `/api/Game/${id}`;
     const response = await axios.delete(url);
     return response.data;
   },
   async addImage(image: FormData) {
-    const url = `${BaseUrl}/api/Game/Image`;
+    const url = `/api/Game/Image`;
     const response = await axios.post(url, image);
     console.log(`Filename: ${response.data}`);
     return response.data;
